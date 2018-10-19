198cf2628b0176979039ef3ba52633ce27542101
Matthew Cam <matthew.cam@gmail.com>
2018-10-18 21:59:08 -0700

    Update Toy_Encryption.hs

--- Toy_Encryption.hs
+++ Toy_Encryption.hs
@@ -9,53 +9,59 @@ module Toy_Encryption where
     import Crypto.Hash
     import qualified Data.ByteString as BYTE
     import Data.ByteString (ByteString)
-    import qualified Data.ByteString.Char8 as WRD
+    import Data.ByteString.Char8 (pack)
+    import Control.Monad
 
-    teststring = WRD.pack "Username"
-    bytestring = BYTE.unpack teststring
+    salt        = generateSalt
+    
+    pre_key     = "pre-key"
+    byte_key    = pack "byte"
 
-    salt = []
-    pre_key = _
-    byte_key = _
-    key = _
+    pre_msg     = "Secret!"
+    byte_msg    = pack "Secret Message!"
+
+    result_byte = pack "byte"
+    result      = "Secret!"
 
     -- Main --
     
     mainEncrypt msg = do
-        salt <- generateSalt
-        pre_key <- generateKey
-        byte_key <- WRD.pack pre_key
-        key <- hashingSHA1 bytekey
-        msg <- WRD.pack msg
-        return encryptMsg key msg
+        let salt = generateSalt
+        let pre_key = generateKey
+        let byte_key = pack pre_key
+        let byte_msg = pack msg
+        encryptMsg byte_key byte_msg
 
-    mainDecrypt msg = do
-        msg <- WRD.pack msg
-        return decryptMsg key msg
+    mainDecrypt encrypted = do
+        return BYTE.unpack (decryptMsg byte_key encrypted)
     
     -- Salt Generation --
 
+    generateSalt :: IO [Char]
     generateSalt = do
         g <- newStdGen
-        return take 64 $ (randomRs ('0','9') g)
+        return (take 64 $ (randomRs ('0','9') g))
 
     -- Key Generation --
 
-    generateKey = do
-         return teststring ++ generateSalt
-
-    hashingSHA1 :: ByteString -> IO ()
-    hashingSHA1 key = do
-        -- putStrLn $ "  sha1(" ++ show key ++ ") = " ++ show (hashWith SHA1   key)
-        return (hashWith SHA1   key)
+    generateKey =
+        liftM (pre_key ++) salt
 
     -- Encryption --
 
-    encryptMsg key msg =
-        encrypt key msg
+    encryptMsg :: ByteString -> ByteString -> IO ByteString
+    encryptMsg key1 msg1 =
+        encrypt key1 msg1
 
     -- Decryption --
 
-    decryptMsg key msg =
-        decrypt key msg
+    decryptMsg :: ByteString -> ByteString -> IO ByteString
+    decryptMsg key2 msg2 =
+        decrypt key2 msg2
+
+    -- SHA256 Encryption --
+
+    hashingSHA256 :: ByteString -> IO()
+    hashingSHA256 byte_msg = do
+        putStrLn $ "sha256( " ++ show byte_msg ++ " ) = " ++ show (hashWith SHA256 byte_msg)
 
